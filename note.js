cb15cea53d0122e39fd5d003731623e2b7d3f9b8
kraiyna2000 <kraiyan58@gmail.com>
2023-10-03 14:52:22 +0530

    first commit

---
+++ note.js
@@ -0,0 +1,46 @@
+let songs = [
+    {
+        name: 'Challeya',
+        path: 'Music/song 1.mp3',
+        artist: 'Mohit Chauhan',
+        cover: '/images/img1.png'
+
+
+    },
+
+    {
+        name: 'Kesariya',
+        path: 'Music/song 2.mp3',
+        artist: 'Arijith sing',
+        cover: '/images/img2.png'
+
+
+    },
+
+    {
+        name: 'Hasi ',
+        path: 'Music/song 3.mp3',
+        artist: 'Ami Mishra',
+        cover: '/images/img3.png'
+
+
+    },
+
+    {
+        name: 'janiye',
+        path: 'Music/song 4.mp3',
+        artist: 'Rashmeet Kaur and Vishal Mishra',
+        cover: '/images/img4.png'
+
+
+    },
+    {
+        name: 'Tu hi hai aashiqui',
+        path: 'Music/song 5.mp3',
+        artist: 'Arijith sing',
+        cover: '/images/img5.png'
+
+
+    },
+]
+
